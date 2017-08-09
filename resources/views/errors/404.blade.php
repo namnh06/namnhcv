a31bb2e2259e88a5d6fc0fe6d5abfe0e8e70eaf3
namnh06 <namnh.website@gmail.com>
2017-08-09 17:14:23 +0700

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('errors.404')
+@section('title','Ooops 404')
+
+@section('content')
+	<div class="content">
+		<div class="container">
+
+			<!-- START: PAGE CONTENT -->
+			<div class="page-404">
+				<h2>4<span>0</span>4</h2>
+				<p>Ooops! This page doesn't even exist</p>
+				<a class="btn btn-lg btn-border" href="index.html">Go To The Homepage ?</a>
+			</div>
+			<!-- END: PAGE CONTENT -->
+
+		</div><!-- .container -->
+	</div><!-- .content -->
+	@endsection
