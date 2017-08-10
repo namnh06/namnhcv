c02a02cce5608067a7e0d692ac2451ff64c0f397
namnh06 <namnh.website@gmail.com>
2017-08-10 12:35:51 +0700

    update og

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -5,7 +5,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<meta name="title" content="Nam Nguyen Hai Resume">
+	<meta name="description" content="Nam's website, this is my resume, check it out !!!">
+	<meta property="og:title" content="Nam Nguyen Hai Resume" />
+	<meta property="og:type" content="Personal Webiste" />
+	<meta property="og:url" content="http://www.namnh.website/" />
 	<title>@yield('title')</title>
 	<base href="{{asset('')}}">
 	<!-- Favicon -->
