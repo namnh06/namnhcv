4b49a353a9b5851b00e2b22084bc4a027403e247
namnh06 <namnh.website@gmail.com>
2017-08-11 15:48:23 +0700

    index page

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -102,11 +102,11 @@
 				<li><a href="#skills">Skills</a></li>
 				<li><a href="#portfolio">Portfolio</a></li>
 				<li><a href="#experience">Experience</a></li>
+				<li><a href="#education">Education</a></li>
 				<li><a href="#references">References</a></li>
 				{{--<li>--}}
 					{{--<a href="#">Blog</a>--}}
 				{{--</li>--}}
-				<li><a href="#calendar">Calendar <span></span></a></li>
 				<li><a href="#contact">Contact <span></span></a></li>
 			</ul>
 		</nav>
@@ -215,13 +215,13 @@
 									<li><a href="#portfolio">Portfolio</a></li>
 									<li><a href="#experience">Experience</a>
 									</li>
+									<li><a href="#education">Education</a>
+									</li>
 									<li><a href="#references">References</a>
 									</li>
 									{{--<li>--}}
 										{{--<a href="#">Blog</a>--}}
 									{{--</li>--}}
-									<li><a href="#calendar">Calendar
-											<span></span></a></li>
 									<li><a href="#contact">Contact <span></span></a>
 									</li>
 								</ul>
